0271069a7c799f4ef79ab5dee1462771079c8775
Marcos Gomes <mpcgomes2@gmail.com>
2024-03-04 15:09:59 -0300

    Spring Boot Setup

---
+++ backend/src/main/java/com/niloware/backend/NilowareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.niloware.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NilowareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NilowareBackendApplication.class, args);
+	}
+
+}
