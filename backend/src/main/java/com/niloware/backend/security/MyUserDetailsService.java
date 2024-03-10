36f6c4b6a21f70a7dff4c0890e4999149548ad5a
Marcos Gomes <mpcgomes2@gmail.com>
2024-03-10 16:23:58 -0300

    MyUserDetailsService

---
+++ backend/src/main/java/com/niloware/backend/security/MyUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.niloware.backend.security;
+
+import org.springframework.security.core.userdetails.User;
+import com.niloware.backend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.niloware.backend.model.User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
